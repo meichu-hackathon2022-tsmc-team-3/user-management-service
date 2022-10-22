3b4393baee2140984e6c833f38a0d651f910cf3f
N0Ball <n0ball@gmail.com>
2022-10-22 22:33:34 +0800

    CHORE: fix minor bugs

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: 3000,
     DB: {
-      PROD: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/musika`,
+      PROD: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}`,
       DEV: `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
       TEST: `mongodb://test-mongo:27017/${process.env.MONGO_DB}`,
     }
